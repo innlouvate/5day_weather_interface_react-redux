ef527f41f925389d53cc3ecdadb5f4dbe3c379d6
Lou <lfranklin@deloitte.co.uk>
2016-06-23 14:43:03 +0100

    functioning googlemap for location

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap} from 'react-ggogle-maps';
+import {GoogleMapLoader, GoogleMap} from 'react-google-maps';
 
 export default (props) => {
   return (
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Sparklines, SparklinesLine} from 'react-sparklines';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(citydata) {
@@ -9,19 +10,14 @@ class WeatherList extends Component {
     const temps = citydata.list.map(weather => (weather.main.temp - 273.15))
     const pressures = citydata.list.map(weather => weather.main.pressure)
     const humidities = citydata.list.map(weather => weather.main.humidity)
+    const {lon, lat} = citydata.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td>
-          <Chart data={temps} color='red' units='C' />
-        </td>
-        <td>
-          <Chart data={pressures} color='blue' units='hPa' />
-        </td>
-        <td>
-          <Chart data={humidities} color='orange' units='%' />
-        </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color='red' units='C' /></td>
+        <td><Chart data={pressures} color='blue' units='hPa' /></td>
+        <td><Chart data={humidities} color='orange' units='%' /></td>
       </tr>
     );
   }
