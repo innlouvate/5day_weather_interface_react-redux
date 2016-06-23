d0e067a83178d6f1ea577ee43d4e3db508408bce
Lou <lfranklin@deloitte.co.uk>
2016-06-23 11:15:41 +0100

    weather api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-const API-KEY='b71ae99713a2a07a82fad0627e1c8512';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&${city},uk`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
